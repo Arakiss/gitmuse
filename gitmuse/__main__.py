96d9af135918dc5060252a5707c6b2367643ad55
Petru Arakiss <petruarakiss@gmail.com>
2024-07-27 01:11:37 +0200

    Add main function to __main__.py for proper entry point

--- gitmuse/__main__.py
+++ gitmuse/__main__.py
@@ -1,8 +1,13 @@
 import sys
 from gitmuse.cli.cli_core import run_cli, run_commit
 
-if __name__ == "__main__":
+
+def main():
     if len(sys.argv) == 1:
         run_commit()
     else:
         run_cli()
+
+
+if __name__ == "__main__":
+    main()
